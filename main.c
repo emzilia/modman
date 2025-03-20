1138819296d13e83f8a2540abe23da3e7f71d810
emzilia <em@zilia.dev>
2025-03-20 17:58:58 -0400

    made inactive/active names more clear

--- main.c
+++ main.c
@@ -13,19 +13,19 @@
 #define BORDER_STARTX 1
 #define BORDER_STARTY 1
 
-#define NOMOD_Y 20
-#define NOMOD_X 29
-#define NOMOD_STARTY 3
-#define NOMOD_STARTX 2
+#define INACTIVE_Y 20
+#define INACTIVE_X 29
+#define INACTIVE_STARTY 3
+#define INACTIVE_STARTX 2
 
-#define MOD_Y 20
-#define MOD_X 29
-#define MOD_STARTY 3
-#define MOD_STARTX 31
+#define ACTIVE_Y 20
+#define ACTIVE_X 29
+#define ACTIVE_STARTY 3
+#define ACTIVE_STARTX 31
 
 WINDOW* border_window;
-WINDOW* mod_window;
-WINDOW* nomod_window;
+WINDOW* active_window;
+WINDOW* inactive_window;
 
 // directory content struct
 typedef struct DirContents {
@@ -40,29 +40,29 @@ typedef struct DirContents {
 DIR *dir;
 struct dirent *entry;
 
-DirContents nomod = {
-	.path = "/home/em/repos/modman/nomods/",
+DirContents inactive = {
+	.path = "/home/em/repos/modman/inactive_mods/",
 	.highlight = 0,
 };
-DirContents mod = {
+DirContents active = {
 	.path = "/home/em/repos/modman/mods/",
 	.highlight = 0,
 };
 
 // function declarations
-void init_window(DirContents* nomodfolder, DirContents* modfolder);
+void init_window(DirContents* inactivefolder, DirContents* activefolder);
 void init_dircontents(DirContents* folder, int count);
 void free_elements(DirContents* folder);
 int get_files(DirContents* folder);
-void refresh_files(DirContents* nomodfolder, DirContents* modfolder);
-int switch_pane(int choice, DirContents* nomodfolder, DirContents* modfolder);
-int change_index(int choice, char* direction, DirContents* nomodfolder, DirContents* modfolder);
+void refresh_files(DirContents* inactivefolder, DirContents* activefolder);
+int switch_pane(int choice, DirContents* inactivefolder, DirContents* activefolder);
+int change_index(int choice, char* direction, DirContents* inactivefolder, DirContents* activefolder);
 void display_panes(int choice, DirContents* folder);
-void display_metadata(int choice, WINDOW* win, DirContents* nomodfolder, DirContents* modfolder);
+void display_metadata(int choice, WINDOW* win, DirContents* inactivefolder, DirContents* activefolder);
 
 // initializes three ncurses windows, one for the background/border
-// and two for the mod/inactive mod folders respectively
-void init_window(DirContents* nomodfolder, DirContents* modfolder) {
+// and two for the active/inactive active folders respectively
+void init_window(DirContents* inactivefolder, DirContents* activefolder) {
 	// init options
 	initscr();
 	noecho();
@@ -75,12 +75,12 @@ void init_window(DirContents* nomodfolder, DirContents* modfolder) {
 	box(border_window, '|', '-');
 	keypad(border_window, TRUE);
 
-	mod_window = newwin(
-			MOD_Y, MOD_X, MOD_STARTY, MOD_STARTX
+	active_window = newwin(
+			ACTIVE_Y, ACTIVE_X, ACTIVE_STARTY, ACTIVE_STARTX
 	);
 
-	nomod_window = newwin(
-			NOMOD_Y, NOMOD_X, NOMOD_STARTY, NOMOD_STARTX
+	inactive_window = newwin(
+			INACTIVE_Y, INACTIVE_X, INACTIVE_STARTY, INACTIVE_STARTX
 	);
 
 	// static text placeholder
@@ -91,12 +91,12 @@ void init_window(DirContents* nomodfolder, DirContents* modfolder) {
 	);
 
 	// DirContents type window assignment
-	nomodfolder->win = nomod_window;
-	modfolder->win = mod_window;
+	inactivefolder->win = inactive_window;
+	activefolder->win = active_window;
 
 	wrefresh(border_window);
-	wrefresh(mod_window);
-	wrefresh(nomod_window);
+	wrefresh(active_window);
+	wrefresh(inactive_window);
 }
 
 // allocates memory for the DirContents files array and each individual
@@ -161,29 +161,29 @@ int get_files(DirContents* folder) {
 	return count;
 };
 
-void refresh_files(DirContents* nomodfolder, DirContents* modfolder) {
-	get_files(nomodfolder);
-	get_files(modfolder);
+void refresh_files(DirContents* inactivefolder, DirContents* activefolder) {
+	get_files(inactivefolder);
+	get_files(activefolder);
 }
 
 // switches from left to right pane, updates directory list to ensure the choice
 // isn't greater than the selected directory index
-int switch_pane(int choice, DirContents* nomodfolder, DirContents* modfolder) {
-	if (nomodfolder->highlight == 1) {
-		modfolder->size = get_files(modfolder);
-		if (modfolder->size == 0) return 0;
-		--nomodfolder->highlight;
-		++modfolder->highlight;
-		if (choice > modfolder->size - 1) {
-			choice = modfolder->size - 1;
+int switch_pane(int choice, DirContents* inactivefolder, DirContents* activefolder) {
+	if (inactivefolder->highlight == 1) {
+		activefolder->size = get_files(activefolder);
+		if (activefolder->size == 0) return 0;
+		--inactivefolder->highlight;
+		++activefolder->highlight;
+		if (choice > activefolder->size - 1) {
+			choice = activefolder->size - 1;
 		}
-	} else if (modfolder->highlight == 1) {
-		nomodfolder->size = get_files(nomodfolder);
-		if (nomodfolder->size == 0) return 0;
-		++nomodfolder->highlight;
-		--modfolder->highlight;
-		if (choice > nomodfolder->size - 1) {
-			choice = nomodfolder->size - 1;
+	} else if (activefolder->highlight == 1) {
+		inactivefolder->size = get_files(inactivefolder);
+		if (inactivefolder->size == 0) return 0;
+		++inactivefolder->highlight;
+		--activefolder->highlight;
+		if (choice > inactivefolder->size - 1) {
+			choice = inactivefolder->size - 1;
 		}
 	}
 	return choice;
@@ -191,16 +191,16 @@ int switch_pane(int choice, DirContents* nomodfolder, DirContents* modfolder) {
 
 // moves selected index up or down depending on the string given, making sure to
 // stay within the bounds of the array
-int change_index(int choice, char* direction, DirContents* nomodfolder, DirContents* modfolder) {
-	if (nomodfolder->highlight == 1) {
+int change_index(int choice, char* direction, DirContents* inactivefolder, DirContents* activefolder) {
+	if (inactivefolder->highlight == 1) {
 		if (strcmp(direction, "up")) {
-			if (choice < nomodfolder->size - 1) ++choice;	
+			if (choice < inactivefolder->size - 1) ++choice;	
 		} else if (strcmp(direction, "down")) {
 			if (choice > 0) --choice;	
 		}
-	} else if (modfolder ->highlight == 1) {
+	} else if (activefolder ->highlight == 1) {
 		if (strcmp(direction, "up")) {
-			if (choice < modfolder->size - 1) ++choice;	
+			if (choice < activefolder->size - 1) ++choice;	
 		} else if (strcmp(direction, "down")) {
 			if (choice > 0) --choice;	
 		}
@@ -270,9 +270,9 @@ void display_panes(int choice, DirContents* folder) {
 	wrefresh(folder->win);
 }
 
-void display_metadata(int choice, WINDOW* win, DirContents* nomodfolder, DirContents* modfolder) {
-	mvwprintw(win, 21, 2, "nomods: %d", nomodfolder->size);
-	mvwprintw(win, 21, 31, "mods: %d", modfolder->size);
+void display_metadata(int choice, WINDOW* win, DirContents* inactivefolder, DirContents* activefolder) {
+	mvwprintw(win, 21, 2, "inactive_mods: %d", inactivefolder->size);
+	mvwprintw(win, 21, 31, "mods: %d", activefolder->size);
 	mvwprintw(win, 25, 2, "choice: %d", choice);
 }
 
@@ -280,51 +280,51 @@ int main() {
 	// choice is what index is currently selected
 	int choice = 0;
 
-	init_window(&nomod, &mod);
+	init_window(&inactive, &active);
 
-	refresh_files(&nomod, &mod);
+	refresh_files(&inactive, &active);
 
-	if (nomod.size > 0) nomod.highlight++;
-	else mod.highlight++;
+	if (inactive.size > 0) inactive.highlight++;
+	else active.highlight++;
 
-	int active = 1;
-	while (active) {
-		refresh_files(&nomod, &mod);
-		display_metadata(choice, border_window, &nomod, &mod);
-		display_panes(choice, &nomod);
-		display_panes(choice, &mod);
+	int running = 1;
+	while (running) {
+		refresh_files(&inactive, &active);
+		display_metadata(choice, border_window, &inactive, &active);
+		display_panes(choice, &inactive);
+		display_panes(choice, &active);
 		int response = wgetch(border_window);
 		switch (response) {
 
 			case 'j':
 			case KEY_DOWN:
-				choice = change_index(choice, "down", &nomod, &mod);
+				choice = change_index(choice, "down", &inactive, &active);
 				break;
 			case 'k':
 			case KEY_UP:
-				choice = change_index(choice, "up", &nomod, &mod);
+				choice = change_index(choice, "up", &inactive, &active);
 				break;
 			case 'h':
 			case KEY_LEFT:
-				choice = switch_pane(choice, &nomod, &mod);
+				choice = switch_pane(choice, &inactive, &active);
 				break;
 			case 'l':
 			case KEY_RIGHT:
-				choice = switch_pane(choice, &nomod, &mod);
+				choice = switch_pane(choice, &inactive, &active);
 				break;
 			case ' ':
-				choice = move_file(choice, &nomod, &mod);	
+				choice = move_file(choice, &inactive, &active);	
 				break;
 			case 'r':
-				refresh_files(&nomod, &mod);
+				refresh_files(&inactive, &active);
 				break;
 			case 'q':
-				active = 0;
+				running = 0;
 				break;
 		}
 	};
 	endwin();
-	free_elements(&nomod);
-	free_elements(&mod);
+	free_elements(&inactive);
+	free_elements(&active);
 	exit(EXIT_SUCCESS);
 }
